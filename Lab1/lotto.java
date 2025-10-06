18bf9852929181c0568a0c71fafe69ac3a51e573
Grzywalski <r1e2p3c4a5k6@gmail.com>
2025-10-06 18:14:41 +0200

    zadanie lotto - wersja 2 poprawiona

--- Lab1/lotto.java
+++ Lab1/lotto.java
@@ -3,12 +3,17 @@ import java.util.Random;
 public class lotto {
 public static void main(String[] args) {
 Random random = new Random();
-int[] wyniki = new int[6];
+float[] wyniki = new float[6];
 for(int i = 0;i<6;i++){
-int randomInt = random.nextInt(49);
-System.out.println("Random int: " + randomInt);
+float randomInt = random.nextFloat(49);
+wyniki[i] = randomInt;
+if(i>=1){
+while(randomInt == wyniki[i-1]){
+randomInt = random.nextFloat(49);
 wyniki[i] = randomInt;
 }
-System.out.println(wyniki);
+}
+System.out.println("Random int: " + randomInt);
+}
 }
 } 
