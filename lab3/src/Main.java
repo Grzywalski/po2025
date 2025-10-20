d9886f455eb7852737429390306b69de606a4032
Grzywalski <r1e2p3c4a5k6@gmail.com>
2025-10-20 18:17:16 +0200

    UnitTesty

--- lab3/src/Main.java
+++ lab3/src/Main.java
@@ -1,15 +1,38 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if(weekday==false || vacation==true){
+            return true;
+        }else{
+            return false;
         }
     }
-}
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if((aSmile ==true && bSmile==true)||(aSmile==false &&bSmile==false)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int sumDouble(int a, int b) {
+        if(a==b){
+            return 4*a;
+        }else{
+            return a+b;
+        }
+    }
+
+
+    public void main(String[] args) {
+
+        System.out.println(sleepIn(true,false));
+
+        System.out.println(monkeyTrouble(true,true));
+
+        System.out.println(sumDouble(3,3));
+        }
+    }
---
+++ lab3/src/MainTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @org.junit.Test
+    public void sleepIn() {
+        System.out.println(Main.sleepIn(false,true));
+        System.out.println(Main.sleepIn(false,false));
+        System.out.println(Main.sleepIn(true,true));
+    }
+
+    @org.junit.Test
+    public void monkeyTrouble() {
+        System.out.println(Main.monkeyTrouble(true,true));
+        System.out.println(Main.monkeyTrouble(false,true));
+        System.out.println(Main.monkeyTrouble(false,false));
+    }
+
+    @org.junit.Test
+    public void sumDouble() {
+        System.out.println(Main.sumDouble(2,2));
+        System.out.println(Main.sumDouble(3,2));
+        System.out.println(Main.sumDouble(4,2));
+    }
+}
+
