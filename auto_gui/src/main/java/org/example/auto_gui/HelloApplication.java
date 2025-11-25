e5c09280c227b432a9be3f7b68ee83d3658d933c
Grzywalski <r1e2p3c4a5k6@gmail.com>
2025-11-25 16:27:56 +0100

    gui- w koncu dziala

--- auto_gui/src/main/java/org/example/auto_gui/HelloApplication.java
+++ auto_gui/src/main/java/org/example/auto_gui/HelloApplication.java
@@ -5,15 +5,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+
+        System.out.println("Classpath root: " + System.getProperty("java.class.path"));
+        System.out.println(HelloApplication.class.getResource("/hello-view.fxml"));
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("hello-view.fxml")
+        );
+
+        Scene scene = new Scene(fxmlLoader.load(), 400, 200);
+        stage.setTitle("Minimal JavaFX App");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
--- auto_gui/src/main/java/org/example/auto_gui/HelloController.java
+++ auto_gui/src/main/java/org/example/auto_gui/HelloController.java
@@ -1,14 +1,74 @@
 package org.example.auto_gui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    // Pola z FXML
+    @FXML private TextField tfNazwa;
+    @FXML private TextField tfIloscBiegow;
+    @FXML private TextField tfAktualnyBieg;
+    @FXML private TextField tfObrotySilnika;
+
+    @FXML private Button btnZwiekszBieg;
+    @FXML private Button btnZmniejszBieg;
+    @FXML private Button btnWcisnijSprzeglo;
+    @FXML private Button btnZwolnijSprzeglo;
+    @FXML private Button btnWlaczSilnik;
+    @FXML private Button btnWylaczSilnik;
+    @FXML private Button btnWlaczAuto;
+    @FXML private Button btnWylaczAuto;
+
+    @FXML private ComboBox<String> comboSamochody;
+
+
+
+    // --------- PRZYKŁADOWE PUSTE METODY ----------
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void initialize() {
+        System.out.println("FXML został poprawnie wczytany.");
+        comboSamochody.getItems().addAll("Auto 1", "Auto 2", "Auto 3");
+    }
+
+    @FXML
+    private void onZwiekszBieg() {
+        System.out.println("Zwiększ bieg");
+    }
+
+    @FXML
+    private void onZmniejszBieg() {
+        System.out.println("Zmniejsz bieg");
+    }
+
+    @FXML
+    private void onWcisnijSprzeglo() {
+        System.out.println("Sprzęgło wciśnięte");
+    }
+
+    @FXML
+    private void onZwolnijSprzeglo() {
+        System.out.println("Sprzęgło zwolnione");
+    }
+
+    @FXML
+    private void onWlaczSilnik() {
+        System.out.println("Włączam silnik");
+    }
+
+    @FXML
+    private void onWylaczSilnik() {
+        System.out.println("Wyłączam silnik");
+    }
+
+    @FXML
+    private void onWlaczAuto() {
+        System.out.println("Auto włączone");
+    }
+
+    @FXML
+    private void onWylaczAuto() {
+        System.out.println("Auto wyłączone");
     }
 }
